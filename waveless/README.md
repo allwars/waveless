c2774f2c1cc65d113c7e6c3c3a36b1c48dad2137
Alvaro Martin <alvaromb8@gmail.com>
2026-03-11 15:15:12 +0100

    Levantamiento proyecto waveless angular 20 SSR

--- waveless/README.md
+++ waveless/README.md
@@ -1,6 +1,6 @@
 # Waveless
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.5.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.19.
 
 ## Development server
 
--- waveless/src/main.server.ts
+++ waveless/src/main.server.ts
@@ -1,5 +1,8 @@
-import {bootstrapApplication, BootstrapContext} from '@angular/platform-browser';
-import {App} from './app/app';
-import {config} from './app/app.config.server';
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
+import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { config } from './app/app.config.server';
+
+const bootstrap = (context: BootstrapContext) =>
+    bootstrapApplication(App, config, context);
+
 export default bootstrap;
